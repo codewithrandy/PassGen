5e462d8d0d346cc10d326bed120d3f4c2b69a1c1
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2023-01-16 12:45:27 -0600

    PEP8 refactoring

--- PassGen.py
+++ PassGen.py
@@ -5,17 +5,17 @@ import random
 import tkinter
 
 # GUI WINDOW SETTINGS
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('blue')
 
 app = customtkinter.CTk()
-app.geometry("500x400")
+app.geometry('500x400')
 app.resizable(False, False)
-app.eval("tk::PlaceWindow . center")
-app.title("Python Password Generator")
+app.eval('tk::PlaceWindow . center')
+app.title('Python Password Generator')
 
 # GLOBAL PASSWORD
-password = ""
+password = ''
 
 
 def get_char_types():
@@ -61,7 +61,7 @@ def get_char(char_types):
 
 def generate_password():
     global password
-    password = ""
+    password = ''
     password_len = int(cb_password_len.get())
     char_types = get_char_types()
     for i in range(password_len):
@@ -69,7 +69,7 @@ def generate_password():
         if len(char_types) > 0:
             password += chr(get_char(char_types))
         else:
-            password = "* * * * * * * *"
+            password = '* * * * * * * *'
     # NOT USING AN ENTRY COMPONENT - TRIM LABEL TO CONFINE TEXT TO BORDER
     lb_password.configure(text=password[:16])
 
@@ -77,63 +77,63 @@ def generate_password():
 def copy_password():
     # COPY PASSWORD TO OS CLIPBOARD
     global password
-    if password != "* * * * * * * *":
+    if password != '* * * * * * * *':
         pyperclip.copy(password)
 
 
 # GUI DESIGN LAYOUT
 # BACKGROUND WALLPAPER
-bg_img = ImageTk.PhotoImage(Image.open("bg.png"))
+bg_img = ImageTk.PhotoImage(Image.open('bg.png'))
 background = customtkinter.CTkLabel(master=app, image=bg_img)
 background.pack()
 
 # INNER FRAME FOR MAIN COMPONENTS
 frame = customtkinter.CTkFrame(master=background,
-                               width=400, height=200, corner_radius=15, bg_color="#1b1b1b")
+                               width=400, height=200, corner_radius=15, bg_color='#1b1b1b')
 frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 # PASSWORD DISPLAY BOX
 lb_password_bd = customtkinter.CTkLabel(master=frame,
                                         width=304, height=49, corner_radius=4,
-                                        fg_color="gray50")
+                                        fg_color='gray50')
 lb_password_bd.place(x=40, y=20)
-lb_password = customtkinter.CTkLabel(master=frame, text="* * * * * * * *",
+lb_password = customtkinter.CTkLabel(master=frame, text='* * * * * * * *',
                                      width=300, height=45, corner_radius=4,
-                                     fg_color="gray15", bg_color="gray50",
-                                     wraplength=0, font=("System", 25))
+                                     fg_color='gray15', bg_color='gray50',
+                                     wraplength=0, font=('System', 25))
 lb_password.place(x=42, y=22)
 
 # CHAR TYPE CHECKBOXES
-chk_upper = customtkinter.CTkCheckBox(master=frame, text="ABC")
+chk_upper = customtkinter.CTkCheckBox(master=frame, text='ABC')
 chk_upper.place(x=50, y=100)
 
-chk_lower = customtkinter.CTkCheckBox(master=frame, text="abc")
+chk_lower = customtkinter.CTkCheckBox(master=frame, text='abc')
 chk_lower.place(x=125, y=100)
 
-chk_digit = customtkinter.CTkCheckBox(master=frame, text="123")
+chk_digit = customtkinter.CTkCheckBox(master=frame, text='123')
 chk_digit.place(x=200, y=100)
 
-chk_symbol = customtkinter.CTkCheckBox(master=frame, text="#&$")
+chk_symbol = customtkinter.CTkCheckBox(master=frame, text='#&$')
 chk_symbol.place(x=275, y=100)
 
 # COMBOBOX FOR PASSWORD LENGTH
-lb_password_len = customtkinter.CTkLabel(master=frame, text="Pass Length")
+lb_password_len = customtkinter.CTkLabel(master=frame, text='Pass Length')
 lb_password_len.place(x=50, y=149)
 cb_password_len = customtkinter.CTkComboBox(master=frame,
                                             width=120,
-                                            values=["16", "32", "64", "128"])
-cb_password_len.set("16")
+                                            values=['16', '32', '64', '128'])
+cb_password_len.set('16')
 cb_password_len.place(x=130, y=150)
 
 # BUTTONS TO GENERATE AND COPY PASSWORD
-copy_icon = customtkinter.CTkImage(Image.open("gen-icon.png").resize((25, 25)))
-bt_generate_pass = customtkinter.CTkButton(master=frame, text="",
+copy_icon = customtkinter.CTkImage(Image.open('gen-icon.png').resize((25, 25)))
+bt_generate_pass = customtkinter.CTkButton(master=frame, text='',
                                            image=copy_icon, width=25,
                                            command=generate_password)
 bt_generate_pass.place(x=260, y=150)
 
-copy_icon = customtkinter.CTkImage(Image.open("copy-icon.png").resize((25, 25)))
-bt_copy_pass = customtkinter.CTkButton(master=frame, text="",
+copy_icon = customtkinter.CTkImage(Image.open('copy-icon.png').resize((25, 25)))
+bt_copy_pass = customtkinter.CTkButton(master=frame, text='',
                                        image=copy_icon, width=25,
                                        command=copy_password)
 bt_copy_pass.place(x=305, y=150)
