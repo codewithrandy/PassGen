d39aed9970602f84aa93a341b66591e9e28e256c
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2023-01-16 09:56:04 -0600

    logic update

--- PassGen.py
+++ PassGen.py
@@ -1,9 +1,10 @@
-import tkinter
 import customtkinter
-import pyperclip
 from PIL import ImageTk, Image
+import pyperclip
 import random
+import tkinter
 
+# GUI WINDOW SETTINGS
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
@@ -13,10 +14,12 @@ app.resizable(False, False)
 app.eval("tk::PlaceWindow . center")
 app.title("Python Password Generator")
 
+# GLOBAL PASSWORD
 password = ""
 
 
 def get_char_types():
+    # RETURN ARRAY OF USER SELECTED CHAR TYPES FROM GUI CHECKBOXES
     types = []
     if chk_upper.get() == 1:
         types.append(1)
@@ -30,13 +33,19 @@ def get_char_types():
 
 
 def get_char(char_types):
+    # CHOOSE A RANDOM USER SELECTED CHAR TYPE AND _
+    # RETURN A DECIMAL IN THAT RANGE TO CONVERT TO ASCII
     match random.choice(char_types):
+        # UPPERCASE
         case 1:
             return random.randint(65, 90)
+        # LOWERCASE
         case 2:
             return random.randint(97, 122)
+        # NUMBERS
         case 3:
             return random.randint(48, 57)
+        # SYMBOLS
         case 4:
             rnd = random.randint(1, 4)
             match rnd:
@@ -56,27 +65,34 @@ def generate_password():
     password_len = int(cb_password_len.get())
     char_types = get_char_types()
     for i in range(password_len):
+        # IF USER HAS NOT SELECTED ANY CHAR TYPES DO NOT GENERATE PASSWORD
         if len(char_types) > 0:
             password += chr(get_char(char_types))
         else:
             password = "* * * * * * * *"
+    # NOT USING AN ENTRY COMPONENT - TRIM LABEL TO CONFINE TEXT TO BORDER
     lb_password.configure(text=password[:16])
 
 
 def copy_password():
+    # COPY PASSWORD TO OS CLIPBOARD
     global password
     if password != "* * * * * * * *":
         pyperclip.copy(password)
 
 
+# GUI DESIGN LAYOUT
+# BACKGROUND WALLPAPER
 bg_img = ImageTk.PhotoImage(Image.open("bg.png"))
 background = customtkinter.CTkLabel(master=app, image=bg_img)
 background.pack()
 
+# INNER FRAME FOR MAIN COMPONENTS
 frame = customtkinter.CTkFrame(master=background,
                                width=400, height=200, corner_radius=15, bg_color="#1b1b1b")
 frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
+# PASSWORD DISPLAY BOX
 lb_password_bd = customtkinter.CTkLabel(master=frame,
                                         width=304, height=49, corner_radius=4,
                                         fg_color="gray50")
@@ -87,6 +103,7 @@ lb_password = customtkinter.CTkLabel(master=frame, text="* * * * * * * *",
                                      wraplength=0, font=("System", 25))
 lb_password.place(x=42, y=22)
 
+# CHAR TYPE CHECKBOXES
 chk_upper = customtkinter.CTkCheckBox(master=frame, text="ABC")
 chk_upper.place(x=50, y=100)
 
@@ -99,6 +116,7 @@ chk_digit.place(x=200, y=100)
 chk_symbol = customtkinter.CTkCheckBox(master=frame, text="#&$")
 chk_symbol.place(x=275, y=100)
 
+# COMBOBOX FOR PASSWORD LENGTH
 lb_password_len = customtkinter.CTkLabel(master=frame, text="Pass Length")
 lb_password_len.place(x=50, y=149)
 cb_password_len = customtkinter.CTkComboBox(master=frame,
@@ -107,6 +125,7 @@ cb_password_len = customtkinter.CTkComboBox(master=frame,
 cb_password_len.set("16")
 cb_password_len.place(x=130, y=150)
 
+# BUTTONS TO GENERATE AND COPY PASSWORD
 copy_icon = customtkinter.CTkImage(Image.open("gen-icon.png").resize((25, 25)))
 bt_generate_pass = customtkinter.CTkButton(master=frame, text="",
                                            image=copy_icon, width=25,
@@ -118,5 +137,6 @@ bt_copy_pass = customtkinter.CTkButton(master=frame, text="",
                                        image=copy_icon, width=25,
                                        command=copy_password)
 bt_copy_pass.place(x=305, y=150)
+
 
 app.mainloop()
